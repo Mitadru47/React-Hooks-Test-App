4a6b863a6278c950e2496f23b88fd7f2505e5a94
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-08-07 22:20:23 +0530

    Established Boilerplate
    Can go in any direction from here.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+
+    <div>
+
+      <h2>Component - App</h2>
+      <p>Welcome to the <strong>App</strong> component. Bit lacklustre ain't it?</p>
+
+    </div>
+  );
+}
+
+export default App;
