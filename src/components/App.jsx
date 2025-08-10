a5fd5337769cb20e48dec2a1fae6c1c68add9c8f
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-08-10 13:53:03 +0530

    State Mutation
    Exploring how to NOT mutate state(s) of primitive and reference-type value(s)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,31 @@ import { Counter } from "./Counter";
 function handleIncrement(event, count, setCount) {
 
   event.preventDefault();
-
   console.log("Before Updating State\nParent Count: " + count);
-  setCount(count + 1);
+
+  // SAFE: Primitives are already immutable
+  // setCount(count + 1);  
+
+  // NOT SAFE: Reference-type values like objects are mutable
+  // NOTE: React won't recognize change in state since, reference remains the same
+
+  // count.counter = count.counter + 1;
+  // setCount(count);
+
+  // NOTE: Creating a new reference-type value will solve the above issue 
+
+  const newCount = { ...count, counter: count.counter + 1 };
+  setCount(newCount);
 
   console.log("After Updating State\nParent Count: " + count);
 }
 
 function App() {
 
-  const [ parentCount, setParentCount ] = useState(0);
+  const [ parentCount, setParentCount ] = useState({ name: "Parent State Count Object", counter: 0 });
   
   useEffect(() => console.log("PARENT COMPONENT FIRST RENDER!"), []);
-  useEffect(() => console.log("PARENT COMPONENT RE-RENDERED!\nParent Count: " + parentCount), [parentCount]);
+  useEffect(() => console.log("PARENT COMPONENT RE-RENDERED!\nParent Count: " + JSON.stringify(parentCount)), [parentCount]);
 
   return (
 
@@ -25,7 +37,7 @@ function App() {
       <h2>Component - App</h2>
       <p>Welcome to the <strong>App</strong> component. Bit lacklustre ain't it?</p>
 
-      <p>{"Parent Count: " + parentCount} <br></br><br></br> <button onClick={(event) => handleIncrement(event, parentCount, setParentCount)}>Increment</button></p>
+      <p>{"Parent Count: " + JSON.stringify(parentCount)} <br></br><br></br> <button onClick={(event) => handleIncrement(event, parentCount, setParentCount)}>Increment</button></p>
       <hr></hr>
 
       <Counter />
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,24 +3,36 @@ import { useEffect, useState } from "react";
 function handleIncrement(event, count, setCount) {
 
   event.preventDefault();
-
   console.log("Before Updating State\nCount: " + count);
-  setCount(count + 1);
+  
+  // SAFE: Primitives are already immutable
+  // setCount(count + 1);  
+
+  // NOT SAFE: Reference-type values like objects are mutable
+  // NOTE: React won't recognize change in state since, reference remains the same
+
+  // count.counter = count.counter + 1;
+  // setCount(count);
+
+  // NOTE: Creating a new reference-type value will solve the above issue 
+
+  const newCount = { ...count, counter: count.counter + 1 };
+  setCount(newCount);
 
   console.log("After Updating State\nCount: " + count);
 }
 
 export function Counter() {
 
-  const [ count, setCount ] = useState(0);
+  const [ count, setCount ] = useState({ name: "Child State Count Object", counter: 0 });
 
   useEffect(() => console.log("CHILD COMPONENT FIRST RENDER!"), []);
-  useEffect(() => console.log("CHILD COMPONENT RE-RENDERED!\nCount: " + count), [count]);
+  useEffect(() => console.log("CHILD COMPONENT RE-RENDERED!\nCount: " + JSON.stringify(count)), [count]);
 
   return(
 
     <>
-      <p>{"Count: " + count} <br></br><br></br> <button onClick={(event) => handleIncrement(event, count, setCount)}>Increment</button></p>
+      <p>{"Count: " + JSON.stringify(count)} <br></br><br></br> <button onClick={(event) => handleIncrement(event, count, setCount)}>Increment</button></p>
 
     </>
   );
